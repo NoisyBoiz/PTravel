783734a2946d964ee09e54f2f2f80f2b95420efc
dungdo25 <126333669+NoisyBoiz@users.noreply.github.com>
2023-04-02 22:41:29 +0700

    change font

--- src/component/footer.js
+++ src/component/footer.js
@@ -7,11 +7,11 @@ function Footer() {
       <div className="socialLinks">
         <div className="socialLinksContent"> Liên hệ: </div>
           <div className="socialLinksContainer">
+          <div className="eachLink instagram"> <i className="fa-brands fa-instagram"></i> </div>
+          <div className="eachLink mail"> <i className="fa-solid fa-envelope"></i></div>
           <div className="eachLink twitter"> <i className="fa-brands fa-twitter"></i> </div>
           <div className="eachLink facebook"> <i className="fa-brands fa-facebook-f"></i> </div>
-          <div className="eachLink instagram"> <i className="fa-brands fa-instagram"></i> </div>
           <div className="eachLink linkedin"> <i className="fa-brands fa-linkedin-in"></i> </div>
-          <div className="eachLink mail"> <i className="fa-solid fa-envelope"></i></div>
         </div>
       </div>
     </div>
